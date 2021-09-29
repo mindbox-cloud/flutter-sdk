ccd41bd899cf84fb130f8e0ab75863a93dc0ef9c
timur.khismatullin <timur.khismatullin@umbrellait.com>
2021-09-29 19:37:12 +0300

    Add base methods

--- mindbox_android/lib/src/mindbox_android_platform.dart
+++ mindbox_android/lib/src/mindbox_android_platform.dart
@@ -1,14 +1,12 @@
-import 'package:flutter/services.dart';
 import 'package:mindbox_platform_interface/mindbox_platform_interface.dart';
 
 /// An [MindboxPlatform] that wraps Mindbox Android SDK.
 class MindboxAndroidPlatform extends MindboxPlatform {
   MindboxAndroidPlatform._();
 
-  static const MethodChannel _channel =
-      MethodChannel('mindbox.cloud/flutter-sdk');
+  final MindboxMethodHandler _methodHandler = MindboxMethodHandler();
 
-  /// Registers this class as the default instance of [InAppPurchasePlatform].
+  /// Registers this class as the default instance of [MindboxPlatform].
   static void registerPlatform() {
     /// Register the platform instance with the plugin platform interface.
     MindboxPlatform.instance = MindboxAndroidPlatform._();
@@ -16,17 +14,29 @@ class MindboxAndroidPlatform extends MindboxPlatform {
 
   /// Returns SDK version or empty string("") on error
   @override
-  Future<String> get sdkVersion async =>
-      await _channel.invokeMethod('getSdkVersion');
+  Future<String> get sdkVersion async => _methodHandler.sdkVersion;
 
   /// Initializes the SDK for further work
   @override
-  Future init({required Configuration configuration}) async {
-    try {
-      await _channel.invokeMethod('init', configuration.toMap());
-    } on PlatformException catch (e) {
-      return Future.error(
-          MindboxException(message: e.message ?? '', details: e.details ?? ''));
-    }
+  Future<void> init({required Configuration configuration}) async {
+    await _methodHandler.init(configuration: configuration);
+  }
+
+  /// Returns device UUID to callback
+  @override
+  void getDeviceUUID({required Function(String uuid) callback}) {
+    _methodHandler.getDeviceUUID(callback: callback);
+  }
+
+  /// Returns token to callback
+  @override
+  void getToken({required Function(String token) callback}) {
+    _methodHandler.getToken(callback: callback);
+  }
+
+  /// Updates FMS token.
+  @override
+  Future<void> updateToken({required String token}) async {
+    await _methodHandler.updateToken(token: token);
   }
 }
