1cd2d810ba01fb1b6af24af11425a6b57c3b1187
timur.khismatullin <timur.khismatullin@umbrellait.com>
2021-10-14 10:45:50 +0300

    Add Objective-C app delegate

---
+++ mindbox_ios/ios/Classes/MindboxFlutterAppDelegate.m
@@ -0,0 +1,65 @@
+#import <MindboxFlutterAppDelegate.h>
+#import <MindboxIosPlugin.h>
+
+@import Mindbox;
+
+@implementation MindboxFlutterAppDelegateObjc
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // MINDBOX INTEGRATION
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self;
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (!error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] registerForRemoteNotifications];
+                [[Mindbox shared] notificationsRequestAuthorizationWithGranted:granted];
+            });
+        }
+        else {
+            NSLog(@"NotificationsRequestAuthorization failed with error: %@", error.localizedDescription);
+        }
+    }];
+    
+    // MINDBOX INTEGRATION
+    if (@available(iOS 13.0, *)) {
+        [[Mindbox shared] registerBGTasks];
+    }
+    else {
+        [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
+    }
+    
+    // MINDBOX INTEGRATION
+    TrackVisitData *data = [[TrackVisitData alloc] init];
+    data.launchOptions = launchOptions;
+    [[Mindbox shared] trackWithData:data];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+// MINDBOX INTEGRATION
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionBadge);
+}
+
+// MINDBOX INTEGRATION
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"token: %@", deviceToken);
+    [[Mindbox shared] apnsTokenUpdateWithDeviceToken:deviceToken];
+}
+
+// MINDBOX INTEGRATION
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+    // SENDING RESPONSE TO FLUTTER
+    [MindboxIosPlugin pushClicked:response];
+    
+    TrackVisitData *data = [[TrackVisitData alloc] init];
+    data.push = response;
+    [[Mindbox shared] trackWithData:data];
+    completionHandler();
+}
+
+// MINDBOX INTEGRATION
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    [[Mindbox shared] application:application performFetchWithCompletionHandler:completionHandler];
+}
+@end
