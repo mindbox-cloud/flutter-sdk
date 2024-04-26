49a60fb51ce242e097c05e54488aef19e4ed8f73
Dmitry Erofeev <56806136+kniksees@users.noreply.github.com>
2024-04-27 02:46:17 +0300

    MBX-3338-Example: add firebase step 2

---
+++ example/flutter_example/android/app/src/main/kotlin/cloud/mindbox/flutter_example/MainApplication.kt
@@ -0,0 +1,12 @@
+package cloud.mindbox.flutter_example
+
+import android.app.Application
+import cloud.mindbox.mobile_sdk.Mindbox
+import cloud.mindbox.mindbox_firebase.MindboxFirebase
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Mindbox.initPushServices(applicationContext, listOf(MindboxFirebase))
+    }
+}
