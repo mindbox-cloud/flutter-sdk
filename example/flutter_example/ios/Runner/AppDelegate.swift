8428337f79ba25079a6a5348d74efc802fe460dd
Dmitry Erofeev <56806136+kniksees@users.noreply.github.com>
2024-04-27 18:56:54 +0300

    MBX-3338-Example: add flutter push request for ios and android

--- example/flutter_example/ios/Runner/AppDelegate.swift
+++ example/flutter_example/ios/Runner/AppDelegate.swift
@@ -4,11 +4,19 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: MindboxFlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    override func shouldRegisterForRemoteNotifications() -> Bool {
+        return false
+    }
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        UIApplication.shared.registerForRemoteNotifications()
+        GeneratedPluginRegistrant.register(with: self)
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
+
--- example/flutter_example/lib/main.dart
+++ example/flutter_example/lib/main.dart
@@ -1,6 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_example/view/main_page.dart';
 import 'package:mindbox/mindbox.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,8 +12,11 @@ void main() {
       endpointAndroid: "Mpush-test.FlutterExample.AndroidApp",
       shouldCreateCustomer: true,
       subscribeCustomerIfCreated: true);
+
   Mindbox.instance.init(configuration: config);
   runApp(const Example());
+
+  requestPermissions();
 
   Mindbox.instance.registerInAppCallback(callbacks: [
     CustomInAppCallback((String id, String redirectUrl, String payload) {
@@ -21,3 +26,16 @@ void main() {
     })
   ]);
 }
+
+
+  Future<void> requestPermissions() async {
+    Permission.notification.isDenied.then((bool isGranted) async {
+      print('==DEBUG==: Requesting permission.');
+      
+      final PermissionStatus status = await Permission.notification.request();
+
+      print('==DEBUG==: New status: $status');
+      Mindbox.instance
+          .updateNotificationPermissionStatus(granted: status.isGranted);
+    });
+  }
