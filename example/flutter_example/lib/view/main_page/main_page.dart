a64fbaf81f88688ad5fd3f00956a3b8087aabba2
Dmitry Erofeev <56806136+kniksees@users.noreply.github.com>
2024-05-16 14:51:29 +0300

    MBX-3338-FullExampleFlutter:add alert

--- example/flutter_example/lib/view/main_page/main_page.dart
+++ example/flutter_example/lib/view/main_page/main_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_example/assets/MBColors.dart';
 import 'package:flutter_example/view/main_page/widgets/buttons_block/buttons_block.dart';
 import 'package:flutter_example/view/main_page/widgets/info_block/info_block.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
@@ -15,6 +16,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+@override
+void initState() {
+
+  super.initState();
+  _showAlertIfNeeded();
+}
+
+Future<void> _showAlertIfNeeded() async {
+  if (!await isAlertShown()) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+
+          content: Text('In-App can only be shown once per session'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setAlertShown();
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
   
   @override
   Widget build(BuildContext context) {
@@ -36,5 +67,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+Future<bool> isAlertShown() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getBool('alertShown') ?? false;
+}
 
-
+Future<void> setAlertShown() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setBool('alertShown', true);
+}
