07052b262402c5b1581f270c3510e44016e833ee
sergeysozinov <sozinov@mindbox.cloud>
2024-10-02 16:27:51 +0300

    MBX-3675: run handleRemoteMessage asynchronously in example

--- example/flutter_example/android/app/src/main/kotlin/cloud/mindbox/flutter_example/MindboxHuaweiMessagingService.kt
+++ example/flutter_example/android/app/src/main/kotlin/cloud/mindbox/flutter_example/MindboxHuaweiMessagingService.kt
@@ -3,8 +3,12 @@ package cloud.mindbox.flutter_example
 import cloud.mindbox.mobile_sdk.Mindbox
 import com.huawei.hms.push.*
 import cloud.mindbox.mindbox_huawei.MindboxHuawei
+import kotlinx.coroutines.*
 
 class MindboxHuaweiMessagingService: HmsMessageService() {
+
+    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Mindbox.updatePushToken(applicationContext, token, MindboxHuawei)
@@ -17,31 +21,40 @@ class MindboxHuaweiMessagingService: HmsMessageService() {
         val channelDescription = "default_channel_description"
         val pushSmallIcon = android.R.drawable.ic_dialog_info
 
-        val messageWasHandled = Mindbox.handleRemoteMessage(
-            context = applicationContext,
-            message = remoteMessage,
-            activities = mapOf(),
-            channelId = channelId,
-            channelName = channelName,
-            pushSmallIcon = pushSmallIcon,
-            defaultActivity = MainActivity::class.java,
-            channelDescription = channelDescription
-        )
+        // On some devices, onMessageReceived may be executed on the main thread
+        // We recommend handling push messages asynchronously
+        coroutineScope.launch {
+            val messageWasHandled = Mindbox.handleRemoteMessage(
+                context = applicationContext,
+                message = remoteMessage,
+                activities = mapOf(),
+                channelId = channelId,
+                channelName = channelName,
+                pushSmallIcon = pushSmallIcon,
+                defaultActivity = MainActivity::class.java,
+                channelDescription = channelDescription
+            )
 
-        // Method for checking if push is from Mindbox
-        val isMindboxPush = MindboxHuawei.isMindboxPush(remoteMessage = remoteMessage)
+            // Method for checking if push is from Mindbox
+            val isMindboxPush = MindboxHuawei.isMindboxPush(remoteMessage = remoteMessage)
 
-        // Method for getting info from Mindbox push
-        val mindboxMessage = MindboxHuawei.convertToMindboxRemoteMessage(remoteMessage = remoteMessage)
-        // If you want to save the notification you can call your save function from here.
-        mindboxMessage?.let {
-            val app = applicationContext as MainApplication
-            app.saveNotification(it)
+            // Method for getting info from Mindbox push
+            val mindboxMessage =
+                MindboxHuawei.convertToMindboxRemoteMessage(remoteMessage = remoteMessage)
+            // If you want to save the notification you can call your save function from here.
+            mindboxMessage?.let {
+                val app = applicationContext as MainApplication
+                app.saveNotification(it)
+            }
+
+            if (!messageWasHandled) {
+                //If the push notification was not from Mindbox or it contains incorrect data, then you can write a fallback to process it
+            }
         }
+    }
 
-        if (!messageWasHandled) {
-            //If the push notification was not from Mindbox or it contains incorrect data, then you can write a fallback to process it
-        }
-
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
     }
 }
