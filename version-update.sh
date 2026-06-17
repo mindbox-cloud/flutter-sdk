c5810848406d6ceba698011f5a563184ce188a6b
sozinov <sozinov@mindbox.cloud>
2026-06-17 12:04:44 +0300

    MOBILE-121: Bump GitHub Actions to supported Node runtimes

--- version-update.sh
+++ version-update.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-VERSION=$1
+#!/usr/bin/env bash
+set -euo pipefail
+VERSION="${1:?usage: version-update.sh <version>}"
 sed -i -e "s|mindbox: ^[0-9].[0-9].[0-9]|mindbox: ^${VERSION}|" README.md
