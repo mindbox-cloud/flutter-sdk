30d7274b87529696234f3dadbc7e3803819b0227
sozinov <sozinov@mindbox.cloud>
2026-06-17 12:59:55 +0300

    MOBILE-121: follow review

--- version-update.sh
+++ version-update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 VERSION="${1:?usage: version-update.sh <version>}"
-sed -i -e "s|mindbox: ^[0-9].[0-9].[0-9]|mindbox: ^${VERSION}|" README.md
+sed -i -e "s|mindbox: ^[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}|mindbox: ^${VERSION}|" README.md
